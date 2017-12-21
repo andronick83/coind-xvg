aee9ab3206fbbb7ac70be29731162856d5013ad2
root <root@lyra.aa>
2017-12-21 20:26:25 +0200

    Fix

--- XVG.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-COINDTAG=xvg
-COIND=${COINDTAG^^}
-COINU=${COINDTAG,,}
-CHOME=var/www/miner/$COINU
-CDATA=$COINHOME/data
-
-sudo -u $COINU /$CHOME/$COIND -conf=/$CHOME/$COIND.conf -datadir=/$CDATA -pid=/$CDATA/$COIND.pid ${@:1}
